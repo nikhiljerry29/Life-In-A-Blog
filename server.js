93f21f708ad95c53028c5fbbfd8d34acffd643b1
nikhiljerry29 <nikhil.gupta5667@gmail.com>
2022-03-16 13:20:59 +0530

    Change port to PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const middlewares = jsonServer.defaults({
    static: "./build",
 });
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 8000;
 server.use(middlewares);
 
 server.use(router);
